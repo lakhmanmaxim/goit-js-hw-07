2ae019244226599fe55ad359bbfb1e0327e9b876
Maxim Lakhman <lakhmanmaxim@gmail.com>
2022-11-12 15:44:43 +0200

    Home work #7 done
    Без закрытия модалки клавишей ESC в первой части

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,77 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// =============================  Создание разметки - НАЧАЛО
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src= ${preview}
+      data-source= ${original}
+      alt= ${description}
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// =============================== Создание разметки - КОНЕЦ
+
+//
+
+// ============ Функции модального окна
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+const refEl = document.querySelector(".gallery__link");
+refEl.addEventListener("click", refElPreventDefault);
+
+function refElPreventDefault(e) {
+  e.preventDefault();
+}
+
+// ============
+
+function onGalleryContainerClick(event) {
+  refElPreventDefault(event);
+
+  const refOriginalImage = event.target.dataset.source;
+  // console.log(refOriginalImage);
+
+  createModal(refOriginalImage);
+}
+
+function createModal(source) {
+  const modal = basicLightbox.create(`
+    <div class="modal">
+        <img
+      class="gallery__image"
+      src="${source}"
+      data-source="${source}"
+      alt=""
+    />
+    </div>
+`);
+
+  modal.show();
+
+  const openModal = document.querySelector(".modal");
+  openModal.addEventListener("click", closeModal);
+
+  function closeModal(e) {
+    modal.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// =============================  Создание разметки - НАЧАЛО
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source= ${original}
+      alt=${description}
+      title=${description}
+    />
+  </a>
+    `;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// =============================== Создание разметки - КОНЕЦ
+
+//================ Модальное окно
+
+const modal = new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
+// ============ Отмена действия по умолчанию - для ссылки
+
+const refEl = document.querySelector(".gallery__item");
+refEl.addEventListener("click", refElPreventDefault);
+
+function refElPreventDefault(e) {
+  e.preventDefault();
+}
